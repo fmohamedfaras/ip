b31dddc00a7ed1f9a7f2575879ce09f8a677c814
fmohamedfaras <fmohamedfaras@gmail.com>
2026-01-31 16:39:39 +0800

    Add echo

--- src/main/java/Olaf.java
+++ src/main/java/Olaf.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Olaf {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Olaf and I like warm hugs!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+           String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+           if (input.equalsIgnoreCase("bye")) {
+               System.out.println("Bye. Hope to see you again soon!");
+               System.out.println("____________________________________________________________");
+               break;
+           }
+           System.out.println(input);
+           System.out.println("____________________________________________________________");
+
+        }
+        scanner.close();
+
     }
 }
