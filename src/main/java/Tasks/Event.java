78e9b10249db63219bdc0a201737d1dc80bd2f00
fmohamedfaras <fmohamedfaras@gmail.com>
2026-03-08 07:15:07 +0800

    Fix save format for Event to Olaf.txt

--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -17,6 +17,6 @@ public class Event extends Task {
 
     @Override
     public String toSaveFormat() {
-        return "E | " + super.toSaveFormat();
+        return "E | " + super.toSaveFormat() + " | " + this.from + " | " + this.to;
     }
 }
